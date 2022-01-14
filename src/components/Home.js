875c791aee560d6c8605f631b75d16d6227efd0c
Martin Veliz <54328819+JMartinVZ3@users.noreply.github.com>
2022-01-14 13:43:34 -0500

    Finished Home Page with loading more button

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -18,11 +19,20 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
 
-    const { state, loading, error, setSearchTerm } = useHomeFetch();
+    const { 
+        state, 
+        loading, 
+        error, 
+        searchTerm,
+        setSearchTerm, 
+        setIsLoadingMore, 
+    } = useHomeFetch();
+
+    if(error) return <div>Something went wrong...</div>
 
     return (
         <>
-        {state.results[0] ?
+        {!searchTerm && state.results[0] ?
         <HeroImage
             image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
             title={state.results[0].original_title}
@@ -31,7 +41,7 @@ const Home = () => {
         : null
         }
         <SearchBar setSearchTerm={setSearchTerm}/>
-        <Grid header='Popular Movies'>
+        <Grid header={searchTerm ? 'Search Result' : "Popular Movies"}>
             {state.results.map(movie => (
                 <Thumb
                     key={movie.id}
@@ -45,7 +55,10 @@ const Home = () => {
                     />
             ))}
         </Grid>
-        <Spinner></Spinner>
+        {loading && <Spinner/>}
+        {state.page < state.total_pages && !loading && (
+            <Button text='Load More' callback={() => setIsLoadingMore(true)}/>
+        )}
         </>
     )
 };
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -15,7 +15,8 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
-
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
+ 
     const fetchMovies = async (page, searchTerm = "") => {
         try {
             setError(false);
@@ -35,10 +36,20 @@ export const useHomeFetch = () => {
         setLoading(false);
     }
 
-    // Initial Render
+    // Initial and Search
     useEffect(() => {
-        fetchMovies(1);
-    }, []);
+        setState(initialState);
+        fetchMovies(1, searchTerm);
+    }, [searchTerm]);
 
-    return { state, loading, error, setSearchTerm };
-}
+    // Load More
+    useEffect(() => {
+        if (!isLoadingMore) return;
+
+        fetchMovies(state.page + 1, searchTerm);
+        setIsLoadingMore(false);
+
+    }, [isLoadingMore, searchTerm, state.page])
+
+    return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
+};
